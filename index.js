da436eeb1a3337928d39407c101009f420e80f90
Henrique Ferreira <henriquef501@gmail.com>
2025-02-18 17:16:09 -0300

    first commit

--- index.js
+++ index.js
@@ -1,32 +1,50 @@
-function gerarQr() {
-  const login = document.querySelector('.input_text-login').value;
-  const senha = document.querySelector('.input_text-senha').value;
-  
-  if (!login || !senha) {
-      alert("Deu ruim");
-      return;
-  }
+function mostrarSecao(secaoId) {
 
-  const wifi = `WIFI:S:${login};T:WPA;P:${senha};;`;
-  console.log(wifi);
+    document.querySelectorAll('.secao').forEach(secao => {
+        secao.classList.remove('ativa');
+    });
 
-  // Limpa o conteúdo anterior do QRCode
-  const qrDiv = document.getElementById('qrcode');
-  qrDiv.innerHTML = ""; 
-
-  // Gera o QR Code
-  QRCode.toDataURL(wifi, {
-      width: 250,
-      height: 250
-  }, function (error, url) {
-      if (error) {
-          console.error(error);
-      } else {
-          console.log("QR Code gerado com sucesso!");
-          // Cria um elemento de imagem e coloca o QR Code gerado nele
-          const img = document.createElement('img');
-          img.src = url;
-          qrDiv.appendChild(img);
-      }
-  });
+    document.getElementById(secaoId).classList.add('ativa');
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    mostrarSecao('url');
+});
+
+function gerarQr() {
+    const login = document.querySelector('.input_text-login').value;
+    const senha = document.querySelector('.input_text-senha').value;
+
+    if (!login || !senha) {
+        alert("Deu ruim");
+        return;
+    }
+
+    const wifi = `WIFI:S:${login};T:WPA;P:${senha};;`;
+    console.log(wifi);
+
+
+    const qrDiv = document.getElementById('qrcode');
+    qrDiv.innerHTML = "";
+
+
+    QRCode.toDataURL(wifi, {
+        width: 250,
+        height: 250,
+        colors: {
+
+        },
+    }, function (error, url) {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log("QR Code gerado com sucesso!");
+
+            const img = document.createElement('img');
+            img.src = url;
+            qrDiv.appendChild(img);
+        }
+    });
+}
+
+
